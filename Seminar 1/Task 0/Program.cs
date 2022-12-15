ae33731b723fa54d860cc2caddbe89926870c8a0
Yana Shushkova <yaya2004@mail.ru>
2022-12-15 23:10:55 +0300

    добавила задачи

---
+++ Seminar 1/Task 0/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 0: Напишите программу которая на вход принимает число и выдает его 
+//  квадрат (число умноженное само на себя)
+/* Например:
+4 -> 16
+-3 -> 9
+-7 -> 49 */
+
+// Запрос числа от пользователя
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(number);
+
+// Квадрат исходного числа
+
+int sqrnumber = number * number;
+
+// Вывод результата на экран
+
+Console.WriteLine($"Квадрат вашего числа = {sqrnumber}");
+
+// Console.WriteLine($"Квадрат вашего числа = {number * number}");
+
