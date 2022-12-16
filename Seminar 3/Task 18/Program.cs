94398af6183a53f1404e45d824c3452f99c4032a
Yana Shushkova <yaya2004@mail.ru>
2022-12-16 16:43:52 +0300

    Seminar 3 add tasks

---
+++ Seminar 3/Task 18/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 18: Напишите программу, которая по заданному номеру 
+четверти, показывает диапазон возможных координат точек в этой 
+четверти (x и y) */
+
+/* WriteLine метод, а в () аргументы, методы позволяют 
+нам выделить какое-то дествие в отдельную программку, 
+которую можно отдельно вызывать по имени*/
+
+// <Возвращаемый тип> <Наименование метода> (переменные) {тело метода}
+
+
+// 
+
+/* void GetXYArea(int chetvert)
+{
+
+    if (chetvert == 1)
+    {
+        Console.WriteLine("cordX> 0 && cordY > 0 ");
+    }
+    if (chetvert == 2)
+    {
+        Console.WriteLine("cordX < 0 && cordY > 0 ");
+    }
+
+    if (chetvert == 3)
+    {
+        Console.WriteLine("cordX < 0 && cordY < 0 ");
+    }
+
+    if (chetvert == 4)
+    {
+        Console.WriteLine("cordX > 0 && cordY < 0");
+    }
+
+}
+
+Console.WriteLine("Введи четверть: ");
+int chetvert = Convert.ToInt32(Console.ReadLine());
+
+GetXYArea (chetvert); */
+
+string GetXYArea(int chetvert)
+{
+
+    if (chetvert == 1)
+    {
+        return "cordX> 0 && cordY > 0 ";
+    }
+    if (chetvert == 2)
+    {
+       return"cordX < 0 && cordY > 0 ";
+    }
+
+    if (chetvert == 3)
+    {
+        return"cordX < 0 && cordY < 0 ";
+    }
+
+    if (chetvert == 4)
+    {
+       return"cordX > 0 && cordY < 0";
+    }
+return "такой четверти нет";
+}
+
+Console.WriteLine("Введи четверть: ");
+int chetvert = Convert.ToInt32(Console.ReadLine());
+
+string rezult = GetXYArea (chetvert);
+/* Console.WriteLine (GetXYArea (chetvert)); */
+Console.WriteLine (rezult);
